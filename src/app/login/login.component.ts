ea6841deeddae33d74b27aee14cc02115307df49
GABBRIELJUA <20221040@uthh.edu.mx>
2024-11-18 11:52:14 -0600

    mensff

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -74,13 +74,7 @@ export class LoginComponent {
             setTimeout(() => {
               this.mensaje = '';
             }, 3000);
-          } else {
-            this.mensaje = 'Error al iniciar sesión: ' + (err.error?.message || 'Servidor no disponible');
-            this.exito = false;
-            setTimeout(() => {
-              this.mensaje = '';
-            }, 3000);
-          }
+          } 
         }
         
       });

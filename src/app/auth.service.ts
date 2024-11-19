d8771ca0441da1d7d4d719d8a121d4c8d057c5b5
GABBRIELJUA <20221040@uthh.edu.mx>
2024-11-19 17:24:49 -0600

    notyes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +7,6 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
   private tipoUsuarioSubject = new BehaviorSubject<string | null>(null);
   tipoUsuario$ = this.tipoUsuarioSubject.asObservable();
-
-  constructor(private http: HttpClient) {}
 
   login(type: string) {
     this.tipoUsuarioSubject.next(type); // Emitir el tipo de usuario
@@ -20,17 +17,8 @@ export class AuthService {
   }
 
   logout() {
+    this.tipoUsuarioSubject.next(null); // Limpiar el tipo de usuario al cerrar sesión
+    localStorage.removeItem('token'); // Limpiar el token
 
-     // Llama al servidor para eliminar la cookie
-     this.http.post('https://back-tienda-livid.vercel.app/api/logout', {}, { withCredentials: true }).subscribe(
-      () => {
-        console.log('Sesión cerrada en el servidor');
-        this.tipoUsuarioSubject.next(null); // Limpiar el tipo de usuario
-        localStorage.removeItem('token'); // Limpiar el token
-      },
-      (error) => {
-        console.error('Error al cerrar sesión:', error);
-      }
-    );
   }
 }

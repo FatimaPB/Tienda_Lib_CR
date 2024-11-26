1079f6601dbe9563ea6d6b9bebddc643a85927fc
GABBRIELJUA <20221040@uthh.edu.mx>
2024-11-26 12:59:01 -0600

    validorsssgf

--- src/app/perfilempresa/perfilempresa.component.ts
+++ src/app/perfilempresa/perfilempresa.component.ts
@@ -36,7 +36,7 @@ export class PerfilempresaComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.empresaForm = this.fb.group({
-      nombre: ['',[ Validators.required, ,Validators.minLength(3)]],
+      nombre: ['',[ Validators.required,Validators.minLength(3)]],
       slogan: ['', [Validators.maxLength(200)]],
       logo: [''], // Este campo puede ser utilizado para el nombre del archivo o URL
       redesSociales: this.fb.group({

31db0a948e9bce4a4d62817a7eb10caf7faa9847
GABBRIELJUA <20221040@uthh.edu.mx>
2025-01-31 04:28:34 -0600

    pageseges

--- src/app/noticias-eventos/noticias-eventos.component.ts
+++ src/app/noticias-eventos/noticias-eventos.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-noticias-eventos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './noticias-eventos.component.html',
   styleUrl: './noticias-eventos.component.css'
 })

7328aedb019246b2b2bd2c075be0d2b2e41ba5a2
GABBRIELJUA <20221040@uthh.edu.mx>
2025-10-30 01:12:02 -0600

    Implementa PWA en Angular con manifest.json, Service Worker y configuración de caché para uso offline

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { provideEventPlugins } from "@taiga-ui/event-plugins";
 import { provideAnimations } from "@angular/platform-browser/animations";
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideAnimations(), provideRouter(routes),
-    provideHttpClient() ,
-    provideAnimationsAsync(), provideAnimationsAsync(), provideEventPlugins()]
+    provideHttpClient(),
+    provideAnimationsAsync(), provideAnimationsAsync(), provideEventPlugins(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
